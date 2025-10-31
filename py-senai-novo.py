486810bb285007811decbef3e2ba1f3cac410fef
WincarMaster <carsanrodrigo@gmail.com>
2025-10-31 20:41:46 -0300

    aula002

---
+++ py-senai-novo.py
@@ -0,0 +1,12 @@
+import pyautogui as py
+py.press('win')
+py.write('chrome')
+py.press('enter')
+py.sleep(2)
+py.write('senai americana')
+py.press('enter')
+py.sleep(4)
+py.click(x=247, y=514)
+py.sleep(4)
+py.click(x=1372, y=895)
+#print(py.position())  # Use this to find the coordinates of the link
